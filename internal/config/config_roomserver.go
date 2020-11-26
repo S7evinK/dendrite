4bf893d897ff7978b383e1da18c0f813cde0057a
Till Faelligen <tfaelligen@gmail.com>
2020-11-26 19:03:36 +0100

    Add roomserver gRPC config

--- internal/config/config_roomserver.go
+++ internal/config/config_roomserver.go
@@ -13,10 +13,14 @@ func (c *RoomServer) Defaults() {
 	c.InternalAPI.Connect = "http://localhost:7770"
 	c.Database.Defaults()
 	c.Database.ConnectionString = "file:roomserver.db"
+	c.InternalAPI.GRPCConnect = "localhost:0"
+	c.InternalAPI.GRPCListen = "localhost:0"
 }
 
 func (c *RoomServer) Verify(configErrs *ConfigErrors, isMonolith bool) {
 	checkURL(configErrs, "room_server.internal_api.listen", string(c.InternalAPI.Listen))
 	checkURL(configErrs, "room_server.internal_ap.bind", string(c.InternalAPI.Connect))
 	checkNotEmpty(configErrs, "room_server.database.connection_string", string(c.Database.ConnectionString))
+	checkNotEmpty(configErrs, "room_server.internal_api.grpc_listen", c.InternalAPI.GRPCListen)
+	checkNotEmpty(configErrs, "room_server.internal_api.grpc_connect", c.InternalAPI.GRPCConnect)
 }
