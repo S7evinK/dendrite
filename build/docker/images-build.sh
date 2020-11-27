24779bdd563cd5cc447b1e473e0602c27c039fee
Till Faelligen <tfaelligen@gmail.com>
2020-11-27 13:21:01 +0100

    bla

--- build/docker/images-build.sh
+++ build/docker/images-build.sh
@@ -8,5 +8,7 @@ echo "Building tag '${TAG}'"
 
 docker build -f build/docker/Dockerfile -t matrixdotorg/dendrite:${TAG} .
 
-docker build -t matrixdotorg/dendrite-monolith:${TAG}  -f build/docker/Dockerfile.monolith .
-docker build -t matrixdotorg/dendrite-polylith:${TAG}  -f build/docker/Dockerfile.polylith .
+#docker build -t registry.s3cr3t.me/s7evink/dendrite-monolith:${TAG}  -f build/docker/Dockerfile.monolith .
+docker build -t registry2.s3cr3t.me/s7evink/dendrite-polylith:${TAG}  -f build/docker/Dockerfile.polylith .
+
+docker build -t registry2.s3cr3t.me/s7evink/dendrite:generatekeys      --build-arg component=generate-keys                     -f build/docker/Dockerfile.generatekeys .
